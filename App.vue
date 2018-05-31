7356b2a0a97c1cd18530b1a59cbaf05eb08ccb33
Arnelle Balane <arnellebalane@gmail.com>
2018-05-31 20:05:10 +0800

    Use actual images for the cards

--- App.vue
+++ App.vue
@@ -5,18 +5,18 @@
             <a href="https://github.com/arnellebalane/vue-scrolling-card-list" target="_blank" rel="noopener noreferrer">github.com/arnellebalane/vue-scrolling-card-list</a>
         </header>
 
-        <ul class="card-list">
-            <li class="card-item" v-intersect="onIntersect"></li>
-            <li class="card-item" v-intersect="onIntersect"></li>
-            <li class="card-item" v-intersect="onIntersect"></li>
-            <li class="card-item" v-intersect="onIntersect"></li>
-            <li class="card-item" v-intersect="onIntersect"></li>
-            <li class="card-item" v-intersect="onIntersect"></li>
-            <li class="card-item" v-intersect="onIntersect"></li>
-            <li class="card-item" v-intersect="onIntersect"></li>
-            <li class="card-item" v-intersect="onIntersect"></li>
-            <li class="card-item" v-intersect="onIntersect"></li>
-        </ul>
+        <div class="card-list">
+            <img src="./images/01.jpg" class="card-item" v-intersect="onIntersect">
+            <img src="./images/02.jpg" class="card-item" v-intersect="onIntersect">
+            <img src="./images/03.jpg" class="card-item" v-intersect="onIntersect">
+            <img src="./images/04.jpg" class="card-item" v-intersect="onIntersect">
+            <img src="./images/05.jpg" class="card-item" v-intersect="onIntersect">
+            <img src="./images/06.jpg" class="card-item" v-intersect="onIntersect">
+            <img src="./images/07.jpg" class="card-item" v-intersect="onIntersect">
+            <img src="./images/08.jpg" class="card-item" v-intersect="onIntersect">
+            <img src="./images/09.jpg" class="card-item" v-intersect="onIntersect">
+            <img src="./images/10.jpg" class="card-item" v-intersect="onIntersect">
+        </div>
     </div>
 </template>
 
@@ -29,8 +29,10 @@
                 if (intersectionRatio >= 1) {
                     target.removeAttribute('style');
                 } else {
-                    target.style.opacity = intersectionRatio / 2 + 0.5;
-                    target.style.transform = `scale(${intersectionRatio / 2 + 0.5})`;
+                    // Clips the ratio within 0.75 - 1.0 range
+                    const ratio = intersectionRatio * 0.25 + 0.75;
+                    target.style.opacity = ratio;
+                    target.style.transform = `scale(${ratio})`;
                 }
             }
         }
@@ -64,6 +66,9 @@
         width: 100%;
         border-radius: 5px;
         background-color: #673AB7;
+        box-shadow:
+            0 15px 35px rgba(40, 40, 40, 0.25),
+            0 5px 15px rgba(0, 0, 0, 0.1);
     }
 
     .card-item::before {
@@ -73,10 +78,10 @@
     }
 
     .card-item:not(:first-child) {
-        margin-top: 30px;
+        margin-top: 20px;
     }
 
     .card-item:not(:last-child) {
-        margin-bottom: 30px;
+        margin-bottom: 20px;
     }
 </style>
